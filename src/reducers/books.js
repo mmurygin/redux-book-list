fe8e3b0f7379dc3e3eeb2c1e81a95e622b7910d3
Murygin Maksim <mail@muryginm.com>
2018-01-08 14:37:05 +0700

    init

---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Ptter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ];
+};
